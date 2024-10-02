9919e386f81f7cef985323a15609c028dc3f85f9
mhzm <mhzmmhzm00000@gmail,com>
2024-10-02 23:54:33 +0330

    for view write a simple show all list of model and for model write first model with person name

--- Person/models.py
+++ Person/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class person(models.Model):
+    name=models.CharField(max_length=50)
+    family=models.CharField(max_length=50)
+    age=models.IntegerField()
+    national_id=models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return self.national_id
--- Person/views.py
+++ Person/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView,DateDetailView,DeleteView,ListView,UpdateView
+from django.contrib.auth import login,logout
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from .models import person
+
 
 # Create your views here.
+class Allperson(ListView):
+    model=person
+    template_name='indexperson.html'
+

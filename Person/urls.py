74f94b578a45e56c8498833bcf6caa54b4c0b31c
mhzm <mhzmmhzm00000@gmail,com>
2024-10-03 23:48:22 +0330

    for view write class for show detail person and in url write a url for detail class

--- Person/urls.py
+++ Person/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Allperson
+from .views import Allperson,Detailperson
 urlpatterns = [
     path('Allperson/',Allperson.as_view()),
+    path('Detailperson/<int:pk>/',Detailperson.as_view()),
+    
     
     
 ]
--- Person/views.py
+++ Person/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView,DateDetailView,DeleteView,ListView,UpdateView
+from django.views.generic import CreateView,DetailView,DeleteView,ListView,UpdateView
 from django.contrib.auth import login,logout
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from .models import person
@@ -9,4 +9,10 @@ from .models import person
 class Allperson(ListView):
     model=person
     template_name='indexperson.html'
+
+class Detailperson(DetailView):
+    model=person   
+    template_name='indexdetail.html'
+
+    
 

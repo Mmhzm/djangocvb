75a5a04884b35b4d63f33f11ce8f54561a11b93f
mhzm <mhzmmhzm00000@gmail,com>
2024-10-04 23:05:17 +0330

    write url for that cbv class writen in view.py

--- Student/urls.py
+++ Student/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
-from .views import showallstudent,logoutToken
+from .views import showallstudent,logoutToken,liststudent,createstudent,updatestudent,deletestudent
 urlpatterns = [
     path('loginapi/',ObtainAuthToken.as_view()),
     path('showall/',showallstudent.as_view()),
-    path('out/',logoutToken.as_view())
+    path('out/',logoutToken.as_view()),
+    path('Liststu/',liststudent.as_view()),
+    path('Createstu/',createstudent.as_view()),
+    path('Deletestu/<int:pk>/',deletestudent.as_view()),
+    path('Updatestu/<int:pk>/',updatestudent.as_view())
+
+
 
 ]
